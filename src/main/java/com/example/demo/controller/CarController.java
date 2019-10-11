0c364f6756b7856105674e6987b37d3fd5d4aa87
Ndahigeze <yesselini19@gmail.com>
2019-10-11 21:54:24 +0200

    First Demo Init

---
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.domain.Car;
+import com.example.demo.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+    @Autowired
+   private CarRepository carRepository;
+
+
+   @RequestMapping(path = "/save",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE,produces =MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> create(@RequestBody Car car){
+       carRepository.save(car);
+      return new ResponseEntity<Object>(car,HttpStatus.OK);
+  }
+
+
+
+    @RequestMapping(path = "/update/{id}",method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE,produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> update(@RequestBody Car car, @PathVariable("id")int id){
+
+       car.setId(id);
+       carRepository.save(car);
+        return new ResponseEntity<Object>(car,HttpStatus.OK);
+    }
+
+
+    @RequestMapping(path = "/delete/{id}",method = RequestMethod.DELETE,consumes = MediaType.APPLICATION_JSON_VALUE,produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> delete(@PathVariable int id){
+        Car c=carRepository.getOne(id);
+        carRepository.delete(c);
+        return new ResponseEntity<Object>("Done",HttpStatus.OK);
+    }
+    
+
+    @RequestMapping(path = "/",method = RequestMethod.GET,produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> findAll(@RequestBody Car car){
+        carRepository.save(car);
+        return new ResponseEntity<Object>(car,HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/example/demo/domain/Car.java
@@ -0,0 +1,46 @@
+package com.example.demo.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+     @Id
+    private int id;
+    private String name;
+    private String model;
+    private String plateNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+}
